4f344b026c99f601e346689276f25e39b12a5f2a
Hans P. Zizold D <hzizold@hotmail.com>
2023-04-27 13:27:11 -0400

    Exercise completed
    The updateBenefitValue was refactored using switch case.
    Copies of the original method and output were added.
    The new drug was added in index.js

--- pharmacy.js
+++ pharmacy.js
@@ -12,55 +12,73 @@ export class Pharmacy {
   }
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
+      switch(this.drugs[i].name) {
+        case 'Herbal Tea':
+          // At the end of each day our system lowers both values for every drug
+          // "Herbal Tea" actually increases in Benefit the older it gets 
           this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
+          this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+          // Benefit increases twice as fast after the expiration date
+          if (this.drugs[i].expiresIn < 0) {
             this.drugs[i].benefit = this.drugs[i].benefit + 1;
           }
-        }
-      }
+          // The Benefit of an item is never more than 50
+          if (this.drugs[i].benefit > 50) {
+            this.drugs[i].benefit = 50;
+          }
+          break;
+        case 'Magic Pill':
+          break;
+        case 'Fervex':
+          // At the end of each day our system lowers both values for every drug
+          // "Fervex" increases in Benefit as its expiration date approaches
+          this.drugs[i].benefit = this.drugs[i].benefit + 1;
+          this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+          // Benefit increases by 2 when there are 10 days or less
+          if (this.drugs[i].expiresIn < 10) {
+            this.drugs[i].benefit = this.drugs[i].benefit + 1;
+          }
+          // Benefit increases by 3 when there are 5 days or less
+          if (this.drugs[i].expiresIn < 5) {
+            this.drugs[i].benefit = this.drugs[i].benefit + 1;
+          }
+          // Benefit drops to 0 after the expiration date
+          if (this.drugs[i].expiresIn < 0) {
+            this.drugs[i].benefit = 0;
+          }
+          // The Benefit of an item is never more than 50
+          if (this.drugs[i].benefit > 50) {
+            this.drugs[i].benefit = 50;
+          }
+          break;
+        case 'Dafalgan':
+          // At the end of each day our system lowers both values for every drug
+          this.drugs[i].benefit = this.drugs[i].benefit - 2;
+          this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+          // Once the expiration date has passed, Benefit degrades twice as fast
+          if (this.drugs[i].expiresIn < 0) {
+            this.drugs[i].benefit = this.drugs[i].benefit - 2;
+          }
+          // The Benefit of an item is never negative.
+          if (this.drugs[i].benefit < 0) {
+            this.drugs[i].benefit = 0;
+          }
+          break;
+        default:
+          // At the end of each day our system lowers both values for every drug
+          this.drugs[i].benefit = this.drugs[i].benefit - 1;
+          this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+          // Once the expiration date has passed, Benefit degrades twice as fast
+          if (this.drugs[i].expiresIn < 0) {
+            this.drugs[i].benefit = this.drugs[i].benefit - 1;
+          }
+          // The Benefit of an item is never negative.
+          if (this.drugs[i].benefit < 0) {
+            this.drugs[i].benefit = 0;
+          }
+          break;
+      }      
     }
-
     return this.drugs;
   }
 }
